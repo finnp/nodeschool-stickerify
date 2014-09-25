c3597621ca470d4e720809f72192854706b9efad
Finn <derfinn@gmail.com>
2014-09-25 16:25:51 +0200

    change text

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var form = $('#settings')
 function changecolors (e) {
   $('#background').style.fill = $('#background-color').value
   $('#foreground').style.fill = $('#foreground-color').value
+  $('#letter-left').textContent = $('#letter-left-input').value
+  $('#letter-middle').textContent =$('#letter-middle-input').value
+  $('#letter-right').textContent = $('#letter-right-input').value
   return false
 }
 
